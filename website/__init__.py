ca85245e2ff637295677da126b0047b1e96a5e75
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-09-02 23:08:24 +0300

    pages + models cerated

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,36 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+import os 
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "1234"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.abspath(DB_NAME)}"
+
+    db.init_app(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    with app.app_context():
+        create_db()
 
     return app
+def create_db():
+    if not os.path.exists('website/' + DB_NAME):
+        try:
+            db.create_all()
+            print("CREATED")
+        except Exception as e:
+            print("Database creation failed:", str(e))
+    else:
+        print("Database already exists")
+
+
+
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods = ["GET", "POST"])
 def Authenticate():
-    return "<p>Login</p>"
+    data = request.form
+    return render_template("login.html", text="Testing", boolean=True)
 
 @auth.route("/logout")
 def logout():
     return "<p>logout</p>"
 
-@auth.route("/signup")
+@auth.route("/signup", methods = ["GET", "POST"])
 def signup():
-    return "<p>signup</p>"
+    if request.method =="POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+    
+        if len(email) <4: 
+            flash("email must be greater than 4 characters.", category="error")
+        elif len(firstName)<2:
+            flash("first name must be greater than 2 characters.", category="error")
+        elif password1 != password2:
+            flash("passwords must match.", category="error")
+        elif len(password1) < 7:
+            flash("password must be greater than 7 characters.", category="error")
+        else:
+            #add user to DB
+            flash("Account created.", category="success")
+
+    return render_template("signup.html")
