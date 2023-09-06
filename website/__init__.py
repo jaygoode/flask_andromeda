e3a77abb555ee3afc9422db2702f2dae0767d8dd
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-09-06 23:06:41 +0300

    added note creation and deletion + html, and login logut signup

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,44 @@
 from flask import Flask
-from .views import views
-from .auth import auth
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-import os 
+import os
+from flask_login import LoginManager
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "1234"
+    app.config["SECRET_KEY"] = "1234"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.abspath(DB_NAME)}"
 
     db.init_app(app)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User, Note
+
     with app.app_context():
         create_db()
 
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
 def create_db():
-    if not os.path.exists('website/' + DB_NAME):
+    if not os.path.exists("website/" + DB_NAME):
         try:
             db.create_all()
             print("CREATED")
@@ -31,6 +46,3 @@ def create_db():
             print("Database creation failed:", str(e))
     else:
         print("Database already exists")
-
-
-
